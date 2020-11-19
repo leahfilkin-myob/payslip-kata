fe96d73b585421a2246d18738749b1ed9f8533d0
Leah Filkin <leah.filkin@myob.com>
2020-11-19 16:07:29 +1300

    first commit

---
+++ PayslipKata/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PayslipKata
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please input your name:");
+            string name = Console.ReadLine();
+            Console.Write("Please input your surname:");
+            string surname = Console.ReadLine();
+            Console.Write("Please enter your annual salary:");
+            int  salary = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter your super rate:");
+            int super = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter your payment start date:");
+            string startDate = Console.ReadLine();
+            Console.Write("Please enter your payment end date:");
+            string endDate = Console.ReadLine();
+
+
+            Console.WriteLine("Your payslip has been generated:");
+
+            Console.WriteLine("Name: " + name + " " + surname);
+            /* 
+            
+            string[] teammates = new[] {"Leah", "Iris", "Samaa"};
+            foreach (var person in teammates)
+            {
+                Console.WriteLine(person);
+
+            }
+            */
+        }
+    }
+}
